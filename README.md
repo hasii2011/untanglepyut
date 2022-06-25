9f899de664766f6251c0a4b8e73c20a84c5e9d88
Humberto Sanchez II <n3875yffbt@privaterelay.appleid.com>
2022-06-24 20:14:10 -0500

    The first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+
+[![Build Status](https://travis-ci.com/hasii2011/PyUt.svg?branch=master)](https://travis-ci.com/hasii2011/PyUt)
+[![Maintenance](https://img.shields.io/badge/Maintained%3F-yes-green.svg)](https://GitHub.com/Naereen/StrapDown.js/graphs/commit-activity)
+[![macOS](https://svgshare.com/i/ZjP.svg)](https://svgshare.com/i/ZjP.svg)
+<img width="34" height="17" src="./src/org/pyut/resources/img/gplv3-with-text-136x68.png"/> 
+
+[![forthebadge made-with-python](http://ForTheBadge.com/images/badges/made-with-python.svg)](https://www.python.org/)
+
+I need a good project description
---
+++ setup.py
@@ -0,0 +1,22 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="untanglepyut",
+    version="0.1.0",
+    author_email='Humberto.A.Sanchez.II@gmail.com',
+    description='XML to Ogl Object Model',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hasii2011/untanglepyut",
+    packages=[
+        'untanglepyut'
+    ],
+    install_requires=['ogl', 'untangle'],
+)
