9b741843292f00ed8662cb2614526dda2dfc0e37
Humberto Sanchez II <n3875yffbt@privaterelay.appleid.com>
2024-02-05 12:34:17 -0600

    <>[]: <Update dependencies>
    []
    
    [https://github.com/hasii2011/untanglepyut/issues/76]

--- src/untanglepyut/UnTanglePyut.py
+++ src/untanglepyut/UnTanglePyut.py
@@ -210,7 +210,6 @@ class UnTanglePyut:
         # fix line feeds
         pyutNote = cast(PyutNote, self._addPyutObjectAttributes(pyutElement=noteElement, pyutObject=pyutNote))
 
-        # TODO:  Update when code-ally-basic has common code
         rawContent:   str = noteElement['content']
         cleanContent: str = rawContent.replace(XML_END_OF_LINE_MARKER, osLineSep)
         pyutNote.content = cleanContent
